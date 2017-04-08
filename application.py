1bcc653f5d1e9903465b1318ec7c3c14fdffc9ea
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-08 08:15:23 +0700

    add spesific page and loop the page (#6)

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ username = input('GitHub username: ')
 password = input('GitHub password: ')
 github_user = input('GitHub user/ organization: ')
 github_repo = input('GitHub repository name: ')
+per_page = int(input('How many pulls fetched per page? (30) ') or "30")
+page = int(input('Pick start page: (0) ') or "0")
 auto_delete = input('Automatic delete merged repo? (y/n): ')
 
 def validate_bool_input(bool_input):
@@ -36,29 +38,37 @@ if not validate_bool_input(auto_delete):
     sys.exit(1)
 
 # create GitHub object
-g = Github(username, password, per_page=100)
+g = Github(username, password, per_page=per_page)
 
 # define repo and pull requests
 print("get repo from {}/{}".format(github_user, github_repo))
 repo = g.get_user(github_user).get_repo(github_repo)
 
-print("get pull requests")
-pulls = repo.get_pulls("closed")
+while True:
+    print("get pull requests from page {}".format(page))
+    pulls = repo.get_pulls("closed").get_page(page)
 
-print("get head refs")
-refs = ["heads/{}".format(pull.head.ref) for pull in pulls]
+    print("get head refs")
+    refs = ["heads/{}".format(pull.head.ref) for pull in pulls]
 
-print("validate {} branche(s)".format(len(refs)))
-check_valids = [check(ref) for ref in refs]
+    # break the loop
+    if len(refs) is 0:
+        print("page {} is empty, done cleaning".format(page))
+        break
 
-print("remove invalid branche(s)")
-valids = [val for val in check_valids if val is not None]
+    print("validate {} branche(s)".format(len(refs)))
+    check_valids = [check(ref) for ref in refs]
 
-print("perform deletion for {} branche(s)".format(len(valids)))
-for valid in valids:
-    if auto_delete is "y":
-        delete(valid)
-    else:
-        is_del = input("Do you want to delete branch " + valid.ref + "? (y/n): ")
-        if is_del is "y":
+    print("remove invalid branche(s)")
+    valids = [val for val in check_valids if val is not None]
+
+    print("perform deletion for {} branche(s)".format(len(valids)))
+    for valid in valids:
+        if auto_delete is "y":
             delete(valid)
+        else:
+            is_del = input("Do you want to delete branch " + valid.ref + "? (y/n): ")
+            if is_del is "y":
+                delete(valid)
+    
+    page += 1
